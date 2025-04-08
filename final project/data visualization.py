ec2154bc2167cc9dc4c72489cd124da907c66e58
Valeria Serratos <valeriaserratos@Valerias-MacBook-Air-396.local>
2025-04-08 16:58:21 -0400

    added an example of bar graph

--- final project/data visualization.py
+++ final project/data visualization.py
@@ -9,4 +9,15 @@ def visualize_scatterplot():
 
 def visualize_bargraoh():
     '''Create a bar graph of the data.'''
+#example of bar chart 
+songs = list(top_songs.keys())[::-1]
+count = list(top_songs.values())[::-1]
+
+plt.figure(figsize=(10, 6))  # adjust as needed
+plt.barh(songs, count, color='blue')
+plt.xlabel('Play counts')
+plt.ylabel('Song Name')
+plt.title('Top 5 Songs')
+plt.tight_layout()  # helps prevent label cut-off
+plt.savefig("top_songs.png")
     pass
